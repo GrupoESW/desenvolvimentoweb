249f14bdb0003b8f442e21f2307b1c19192bb475
Nilton Junior <niltoncgti@marica.rj.gov.br>
2023-05-10 07:41:30 -0300

    docker compose no ci

---
+++ public_html/verifica.js
@@ -0,0 +1,52 @@
+// verifica se o email recebido está no formato correto //
+function verificaemail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+function enviardados(){
+var telefone = document.dados.cpf.value;
+var regex = new RegExp('^\\([0-9]{2}\\)((3[0-9]{3}-[0-9]{4})|(9[0-9]{3}-[0-9]{5}))$');
+
+if(regex.test(telefone))
+{
+alert( "Preencha campo telefone corretamente!" );
+document.dados.cpf.focus();
+return false;
+}
+
+if(document.dados.cpf.value=="" ||
+document.dados.cpf.value.length != 11)
+{
+alert( "Preencha campo cpf corretamente!" );
+document.dados.cpf.focus();
+return false;
+}
+
+
+if( document.dados.email.value=="" ||
+document.dados.email.value.indexOf('@')==-1 ||
+document.dados.email.value.indexOf('.')==-1 )
+{
+alert( "Preencha campo E-MAIL corretamente!" );
+document.dados.email.focus();
+return false;
+}
+
+if (document.dados.tx_mensagem.value=="")
+{
+alert( "Preencha o campo MENSAGEM!" );
+document.dados.tx_mensagem.focus();
+return false;
+}
+
+if (document.dados.tx_mensagem.value.length < 50 )
+{
+alert( "É necessario preencher o campo MENSAGEM com
+mais de 50 caracteres!" );
+document.dados.tx_mensagem.focus();
+return false;
+}
+
+return true;
+}
